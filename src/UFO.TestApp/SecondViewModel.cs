7c8266e6d5d3ff72d680fb8695aa994e4449e090
Valon Kastrati <valon.k@outlook.at>
2022-07-19 19:35:20 +0200

    wip test app

--- src/UFO.TestApp/SecondViewModel.cs
+++ src/UFO.TestApp/SecondViewModel.cs
@@ -1,20 +1,42 @@
-﻿using UFO.UI.Dialogs;
+﻿using System.Windows.Input;
+using Java.Util.Jar;
+using UFO.UI.Dialogs;
 
 namespace UFO.TestApp
 {
-	public class SecondViewModel
+	public class SecondViewModel : BaseViewModel
 	{
 		private readonly IDialog _dialogService;
+		private bool _isChecked;
 
 		public SecondViewModel(IDialog dialogService)
 		{
 			_dialogService = dialogService;
+			TestCommand = new Command(OnTest);
 		}
-		
-		
+
+		private void OnTest(object obj)
+		{
+			IsChecked = !IsChecked;
+		}
+
+		public ICommand TestCommand { get; set; }
+
+		public string Text { get; set; }
+
+		public bool IsChecked
+		{
+			get => _isChecked;
+			set
+			{
+				_isChecked = value;
+				OnPropertyChanged(nameof(IsChecked));
+			}
+		}
+
 		public async void OnAppearing()
 		{
-			await _dialogService.ShowConfirmDialogAsync("hi", "desc");
+			// await _dialogService.ShowConfirmDialogAsync("This is some Title", "This is a long description text, this is some long text This is a long description text, this is some long text This is a long description text, this is some long text This is a long description text, this is some long text");
 		}
 	}
 }
