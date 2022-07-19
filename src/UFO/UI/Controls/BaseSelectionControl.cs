2c5b5144de54ebea25fadf19962bf80f12bb941c
Valon Kastrati <valon.k@outlook.at>
2022-07-19 19:35:09 +0200

    wip checkbox

---
+++ src/UFO/UI/Controls/BaseSelectionControl.cs
@@ -0,0 +1,40 @@
+﻿namespace UFO.UI.Controls;
+
+public abstract class BaseSelectionControl : ContentView
+{
+    public static readonly BindableProperty IsCheckedProperty = 
+        BindableProperty.Create(nameof(IsChecked), typeof(bool), typeof(UfoCheckBox));
+    
+    public bool IsChecked
+    {
+        get => (bool)GetValue(IsCheckedProperty);
+        set => SetValue(IsCheckedProperty, value);
+    }
+
+	public static readonly BindableProperty TextProperty =
+	   BindableProperty.Create(nameof(Text), typeof(string), typeof(UfoCheckBox));
+
+	public string Text
+	{
+		get => (string)GetValue(TextProperty);
+		set => SetValue(TextProperty, value);
+	}
+
+	public static readonly BindableProperty FontFamilyProperty =
+	   BindableProperty.Create(nameof(FontFamily), typeof(string), typeof(UfoCheckBox), defaultValue: string.Empty);
+
+	public string FontFamily
+	{
+		get => (string)GetValue(FontFamilyProperty);
+		set => SetValue(FontFamilyProperty, value);
+	}
+
+	public static readonly BindableProperty TextColorProperty =
+		BindableProperty.Create(nameof(TextColor), typeof(Color), typeof(UfoCheckBox), defaultValue: Colors.Black);
+
+	public Color TextColor
+	{
+		get => (Color)GetValue(TextColorProperty);
+		set => SetValue(TextColorProperty, value);
+	}
+}
---
+++ src/UFO/UI/Controls/UfoCheckBox.cs
@@ -0,0 +1,50 @@
+﻿namespace UFO.UI.Controls;
+
+public class UfoCheckBox : BaseSelectionControl
+{
+	public UfoCheckBox()
+    {
+        CreateControl();
+    }
+
+    private Label CreateLabel()
+    {
+	    var label = new Label
+	    {
+		    HorizontalTextAlignment = TextAlignment.Start,
+		    VerticalTextAlignment = TextAlignment.Center
+	    };
+	    
+	    label.SetBinding(Label.TextProperty, TextProperty.PropertyName);
+	    label.SetBinding(Label.TextColorProperty, TextColorProperty.PropertyName);
+	    label.SetBinding(Label.FontFamilyProperty, FontFamilyProperty.PropertyName);
+
+		return label;
+    }
+
+    private static CheckBox CreateCheckBox()
+    {
+	    var checkBox = new CheckBox();
+	    checkBox.SetBinding(CheckBox.IsCheckedProperty, IsCheckedProperty.PropertyName);
+
+	    return checkBox;
+    }
+    
+    private void CreateControl()
+    {
+	    var containerGrid = new Grid();
+	    var label = CreateLabel();
+	    var checkBox = CreateCheckBox();
+	    
+	    containerGrid.ColumnDefinitions.Add(new(GridLength.Auto));
+	    containerGrid.ColumnDefinitions.Add(new(GridLength.Star));
+
+	    containerGrid.Children.Add(checkBox);
+	    containerGrid.Children.Add(label);
+
+		Grid.SetColumn(checkBox, 0);
+		Grid.SetColumn(label, 1);
+
+		Content = containerGrid;
+	}
+}
